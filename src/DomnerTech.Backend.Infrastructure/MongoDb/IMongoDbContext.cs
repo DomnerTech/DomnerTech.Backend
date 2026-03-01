239fb25c834bdb136120b9c97cf35623c474df87
codewithmecoder <yaty1031@gmail.com>
2026-03-01 18:20:30 +0700

    fix disposing db context

--- src/DomnerTech.Backend.Infrastructure/MongoDb/IMongoDbContext.cs
+++ src/DomnerTech.Backend.Infrastructure/MongoDb/IMongoDbContext.cs
@@ -3,27 +3,19 @@ using MongoDB.Driver;
 
 namespace DomnerTech.Backend.Infrastructure.MongoDb;
 
-public interface IMongoDbContext : IDisposable
+public interface IMongoDbContext
 {
     IMongoDatabase Database { get; }
 }
 
 public sealed class MongoDbContext : IMongoDbContext
 {
-    private readonly MongoClient _client;
-
     public IMongoDatabase Database { get; }
 
     public MongoDbContext(MongoDbConfig mongoDbConfig)
     {
         var clientSetting = MongoClientSettings.FromConnectionString(mongoDbConfig.ConnectionUri);
-        _client = new MongoClient(clientSetting);
-        Database = _client.GetDatabase(mongoDbConfig.Database);
-    }
-
-    public void Dispose()
-    {
-        _client.Dispose();
-        GC.SuppressFinalize(this);
+        var client = new MongoClient(clientSetting);
+        Database = client.GetDatabase(mongoDbConfig.Database);
     }
 }
